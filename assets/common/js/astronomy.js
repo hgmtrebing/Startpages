a956df003852a0ae0c9b24365a809ce38662077a
hgmtrebing <hgmtrebing@outlook.com>
2018-02-25 17:58:36 -0500

    Large changes in the styling (main.css) of the website

--- assets/common/js/astronomy.js
+++ assets/common/js/astronomy.js
@@ -2,6 +2,7 @@ $("document").ready(function() {
     var long = -77.3;
     var lat = 33.8;
     var datetime = new Date();
+    var prefTimeFormat = "HH:mm:ss"; //for moment.js
     
     var sunTimes = SunCalc.getTimes (datetime, lat, long);
     var sunPosition = SunCalc.getPosition (datetime, lat, long)
@@ -11,13 +12,13 @@ $("document").ready(function() {
     
     var targetDom = $("#div-astronomy");
     var html = `
-<p>Sunrise: ${sunTimes.sunrise}</p>
-<p>Sunset: ${sunTimes.sunset}</p>
-<p>Solar Noon: ${sunTimes.solarNoon}</p>
-<p>Moon Position: ${moonPosition.distance} kilometers</p>
-<p>Moon Phase: ${moonIllumination.phase} </p>
-<p>Moon Rise: ${moonTimes.rise}</p>
-<p>Moon Set: ${moonTimes.set}</p>
+<p>Sunrise: ${ moment(sunTimes.sunrise).format(prefTimeFormat)}</p>
+<p>Sunset: ${ moment(sunTimes.sunset).format(prefTimeFormat)}</p>
+<p>Solar Noon: ${ moment(sunTimes.solarNoon).format(prefTimeFormat)}</p>
+<p>Moon Position: ${moonPosition.distance.toFixed(2)} kilometers</p>
+<p>Moon Illumination: ${(moonIllumination.phase*100).toPrecision(4)}% </p>
+<p>Moon Rise: ${ moment(moonTimes.rise).format(prefTimeFormat)}</p>
+<p>Moon Set: ${ moment(moonTimes.set).format(prefTimeFormat)}</p>
 `;
     
     targetDom.html(html);
